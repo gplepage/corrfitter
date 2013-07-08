838fed04d7246138dc97a2a1ddcc78efc95237aa
G. Peter Lepage <g.p.lepage@cornell.edu>
2013-07-08 14:20:54 -0400

    prep for upload

--- corrfitter.py
+++ corrfitter.py
@@ -58,7 +58,7 @@ import math
 import collections
 import copy
 import time
-__version__ = '3.5.1'
+__version__ = '3.5.1+'
 
 if not hasattr(collections,'OrderedDict'):
     # for older versions of python
